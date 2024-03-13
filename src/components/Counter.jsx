f8da80865be2c67b8149ce0d1c437553377cb6d9
Rasibul <rasibul179@gmail.com>
2024-03-13 14:43:58 +0600

    consuming the sate,action and call dispatch redux hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
 
 
 const Counter = () => {
+    const {count} = useSelector((state)=> state.counter)
+    const disPatch = useDispatch()
+    // console.log(count)
     return (
         <div>
-            
+            <button onClick={() => disPatch(increment())} className="bg-blue-800 px-4 py-4">Increment</button>
+            <h1>{count}</h1>
+            <button onClick={() => disPatch(decrement())} className="bg-purple-800 px-4 py-4">Decrement</button>
         </div>
     );
 };
